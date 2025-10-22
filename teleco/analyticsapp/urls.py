03ef1448c5e44c23a15a17db0290ce43f63be39c
Gaamangwe Victor Morapedi <44627219+gopolanggaba@users.noreply.github.com>
2025-10-22 11:30:53 +0200

    Revert "Checkpoint before follow-up message (#1)"
    This reverts commit 9ef1fe55a7b561be7501eb209f1e10bfe05e82e5.

--- teleco/analyticsapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('revenue-trend-month/', views.revenue_trend_month, name='revenue-trend-month'),
-    path('leaderboard/', views.leaderboard, name='leaderboard'),
-]
--- teleco/cdr/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CdrConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cdr'
