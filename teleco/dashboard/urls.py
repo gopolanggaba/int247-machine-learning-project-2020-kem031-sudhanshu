2b19625967dd79e13feaecb32f4f641adef1571b
Gaamangwe Victor Morapedi <44627219+gopolanggaba@users.noreply.github.com>
2025-10-22 11:30:53 +0200

    Revert "Checkpoint before follow-up message (#1)"
    This reverts commit 9ef1fe55a7b561be7501eb209f1e10bfe05e82e5.

--- teleco/dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from . import views_hvc
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('hvc/', views_hvc.hvc_lists, name='hvc-lists'),
-]
